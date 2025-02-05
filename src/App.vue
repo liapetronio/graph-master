29d8336a30a6613eeea762a378d67c0c48b8a87d
liapetronio <lia.marie.petronio@gmail.com>
2025-02-05 17:31:53 -0500

    added page routes via navbar. component for sphere composition with mock code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>graph master's mania</v-toolbar-title>
+      <v-spacer></v-spacer>
+     
+        <v-tabs>
+          <v-tab to="/">Home</v-tab>
+          <v-tab to="/page-01">Page 01</v-tab>
+          <v-tab to="/page-02">Page 02</v-tab>
+
+        </v-tabs>
+
+
+    </v-app-bar>
     <router-view />
   </v-app>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <HelloWorld />
+  <div>
+    <h1>Home</h1>
+    <p>This is the home page.</p>
+  </div>
 </template>
 
 <script setup>
